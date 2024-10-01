cf21fb4ebb7e8be9316cc2a76e095d5436db157e
levisdev <khoa05032006@gmail.com>
2024-10-01 12:25:33 +0700

    add telescope keybiding

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -68,6 +68,7 @@ return {
           i = {
             ["<C-j>"] = actions.move_selection_next,
             ["<C-k>"] = actions.move_selection_previous,
+            ["<C-c>"] = actions.close,
           },
         },
       })
