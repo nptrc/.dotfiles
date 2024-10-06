ff881816e748f0db93c07447f09f4c7094a9bffe
levisdev <khoa05032006@gmail.com>
2024-10-06 07:54:01 +0700

    disable mini.ai plugin

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -4,6 +4,7 @@ for _, plugin in pairs({
   { "rcarriga/nvim-notify" },
   { "nvimdev/dashboard-nvim" },
   { "echasnovski/mini.pairs" },
+  { "echasnovski/mini.ai" },
   { "folke/tokyonight.nvim" },
 }) do
   table.insert(plugins, vim.tbl_extend("force", plugin, { enabled = false }))
