3c7a7de5705c8168d8a543deb32c26d8444a575d
levisdev <khoa05032006@gmail.com>
2024-10-07 22:42:11 +0700

    update

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -6,6 +6,10 @@ for _, plugin in pairs({
   { "echasnovski/mini.pairs" },
   { "echasnovski/mini.ai" },
   { "folke/tokyonight.nvim" },
+  { "MagicDuck/grug-far.nvim" },
+  { "folke/trouble.nvim" },
+  { "folke/todo-comments.nvim" },
+  { "mfussenegger/nvim-lint" },
 }) do
   table.insert(plugins, vim.tbl_extend("force", plugin, { enabled = false }))
 end
