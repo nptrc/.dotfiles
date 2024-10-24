114e15c6d95fb455926bbdcb37f40e317359398e
levisdev <khoa05032006@gmail.com>
2024-10-24 11:01:07 +0700

    add mini.ai and mini.surround plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -31,4 +31,18 @@ return {
     event = "BufWritePre",
     opts = require "configs.conform",
   },
+
+  {
+    "echasnovski/mini.surround",
+    event = { "BufReadPost", "BufNewFile" },
+    version = false,
+    opts = true,
+  },
+
+  {
+    "echasnovski/mini.ai",
+    event = { "BufReadPost", "BufNewFile" },
+    version = false,
+    opts = true,
+  },
 }
