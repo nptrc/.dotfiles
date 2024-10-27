c0d0df75c075af09d9f2c2a12cb610b03948a5c2
levisdev <khoa05032006@gmail.com>
2024-10-27 10:34:41 +0700

    refectoring cmp.lua

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,25 +3,25 @@ return {
   opts = function(_, conf)
     local cmp = require "cmp"
 
-    conf.mapping = vim.tbl_deep_extend("force", conf.mapping, {
-      ["<C-j>"] = cmp.mapping.select_next_item(),
-      ["<C-k>"] = cmp.mapping.select_prev_item(),
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if require("luasnip").expand_or_jumpable() then
-          require("luasnip").expand_or_jump()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if require("luasnip").jumpable(-1) then
-          require("luasnip").jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
+    return vim.tbl_deep_extend("force", conf, {
+      mapping = {
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if require("luasnip").expand_or_jumpable() then
+            require("luasnip").expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if require("luasnip").jumpable(-1) then
+            require("luasnip").jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      },
     })
-
-    return conf
   end,
 }
