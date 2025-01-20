99663ca31bd3baaf30ab9b8ca15ced05100968e9
levisdev <khoa05032006@gmail.com>
2025-01-20 08:42:04 +0700

    nvim: set kitty padding on enter

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,12 @@ vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
     end
   end,
 })
+
+-- disable kitty padding
+vim.api.nvim_create_autocmd({ "VimEnter", "BufRead" }, {
+  command = ":silent !kitty @ set-spacing padding-left=0 padding-top=0",
+})
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  command = ":silent !kitty @ set-spacing padding-left=default padding-top=default",
+})
