f92ee0951c2f1c4115ea8789d2b35c36edcf9745
nptrc <khoa05032006@gmail.com>
2025-02-07 08:02:50 +0700

    nvim: update

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,6 +2,5 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "MagicDuck/grug-far.nvim", enabled = false },
   { "mfussenegger/nvim-lint", enabled = false },
-  { "p00f/clangd_extensions.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
 }
