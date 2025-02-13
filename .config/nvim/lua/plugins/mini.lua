60082ae16fcd10f56ecabc2c939eb75580a539c5
nptrc <khoa05032006@gmail.com>
2025-02-14 06:12:02 +0700

    nvim: switch to autopairs-nvim

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
     "echasnovski/mini.nvim",
     event = "VeryLazy",
     config = function()
-      require("mini.pairs").setup()
       require("mini.ai").setup()
       require("mini.surround").setup()
       require("mini.icons").setup()
