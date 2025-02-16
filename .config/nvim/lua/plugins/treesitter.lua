acc52fdd1c4f7cdfca94bf29392fecb9b944a5e0
nptrc <khoa05032006@gmail.com>
2025-02-16 10:00:30 +0700

    nvim: only load autotag plugin on filetype

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,41 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufRead", "BufNewFile" },
-  dependencies = {
-    { "windwp/nvim-ts-autotag", opts = {} },
-  },
-  opts = {
-    ensure_installed = "all",
-    indent = { enable = true },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufRead", "BufNewFile" },
+    opts = {
+      ensure_installed = "all",
+      indent = { enable = true },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
     },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
   },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "astro",
+      "glimmer",
+      "handlebars",
+      "html",
+      "javascript",
+      "jsx",
+      "liquid",
+      "markdown",
+      "php",
+      "rescript",
+      "svelte",
+      "tsx",
+      "twig",
+      "typescript",
+      "vue",
+      "xml,",
+    },
+    opts = {},
+  },
 }
