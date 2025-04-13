d7145b2cfd36d331886f93569be6259473715c32
nptrc <khoa05032006@gmail.com>
2025-04-13 20:34:33 +0700

    setup for esp-idf dev

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,31 @@
+local function setup_clangd_cmd()
+  local esp_idf_path = os.getenv("IDF_PATH")
+  local clangd_opts = { "clangd" }
+
+  if esp_idf_path then
+    clangd_opts = {
+      "/home/nptrc/.espressif/tools/esp-clang/esp-18.1.2_20240912/esp-clang/bin/clangd",
+      "--query-driver=**",
+    }
+  end
+
+  return vim.tbl_extend("keep", clangd_opts, {
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--function-arg-placeholders",
+    "--fallback-style=llvm",
+  })
+end
+
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      clangd = {
+        cmd = setup_clangd_cmd(),
+      },
+    },
+  },
+}
