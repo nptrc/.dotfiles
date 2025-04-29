df6ef586c20094ebc486e3edb29e4e45ec5b6efc
nptrc <khoa05032006@gmail.com>
2025-04-30 06:38:16 +0700

    nvim: enabled flash.nvim

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "folke/tokyonight.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "MagicDuck/grug-far.nvim", enabled = false },
   { "mfussenegger/nvim-lint", enabled = false },
