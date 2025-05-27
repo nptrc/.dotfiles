f447d610c9d22ea3255a73c4a5503fe485bdc11b
nptrc <khoa05032006@gmail.com>
2025-05-27 12:19:05 +0700

    nvim: remove flutter support and neovide

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,18 +4,3 @@ _G.dd = function(...)
   Snacks.debug.inspect(...)
 end
 vim.print = _G.dd
-
-if vim.g.neovide then
-  vim.opt.guifont = "FiraCode Nerd Font:h14"
-  vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_remember_window_size = true
-
-  vim.keymap.set("n", "<c-s-v>", "p")
-  vim.keymap.set("n", "<f11>", function()
-    if vim.g.neovide_fullscreen == true then
-      vim.g.neovide_fullscreen = false
-    else
-      vim.g.neovide_fullscreen = true
-    end
-  end)
-end
