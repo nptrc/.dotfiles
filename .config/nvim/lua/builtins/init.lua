124c303a71fcc0173869f23ba3267b754d8296e8
nptrc <khoa05032006@gmail.com>
2025-07-02 22:40:39 +0700

    nvim: refactor config

--- .config/nvim/lua/builtins/init.lua
+++ .config/nvim/lua/builtins/init.lua
@@ -1,8 +1,9 @@
+local H = require("builtins.helpers")
 local function loadPlugin()
   local builtins_dir = vim.fn.stdpath("config") .. "/lua/builtins/"
   for _, plug in ipairs(vim.fn.readdir(builtins_dir)) do
     local path = builtins_dir .. plug
-    if vim.fn.isdirectory(path) == 1 and vim.fn.filereadable(path .. "/init.lua") == 1 then
+    if vim.fn.isdirectory(path) == 1 and H.file_exists("init.lua", path) then
       local module_name = "builtins." .. plug
       package.loaded[module_name] = nil
       require(module_name)
@@ -10,11 +11,8 @@ local function loadPlugin()
   end
 end
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = loadPlugin,
-})
-
+loadPlugin()
 vim.api.nvim_create_user_command("ReloadPlugin", function()
   loadPlugin()
+  H.notify("Reloaded all builtin plugins")
 end, { nargs = 0 })
