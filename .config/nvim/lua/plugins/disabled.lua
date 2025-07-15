9fe2f2dd1b43ce5aab8fe207e1bc9cfbbb00e2ee
nptrc <khoa05032006@gmail.com>
2025-07-15 11:14:18 +0700

    remove clangd and update plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "folke/ts-comments.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "mfussenegger/nvim-lint", enabled = false },
+  { "p00f/clangd_extensions.nvim", enabled = false },
 }
