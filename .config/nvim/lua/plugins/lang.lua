114020af1f2293614662cfc32eabd0f64049cfeb
nptrc <khoa05032006@gmail.com>
2025-07-15 09:45:40 +0700

    nvim: set compile_commands.json in project's root

--- .config/nvim/lua/plugins/lang.lua
+++ .config/nvim/lua/plugins/lang.lua
@@ -3,7 +3,6 @@ return {
     "Civitasv/cmake-tools.nvim",
     opts = {
       cmake_build_directory = "build",
-      cmake_compile_commands_options = { action = "lsp" },
       cmake_generate_options = { "-GNinja", "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
     },
   },
