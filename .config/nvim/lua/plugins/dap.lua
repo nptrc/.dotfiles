dfb4f4b3d028278e798f569b4ff55b1214341263
nptrc <khoa05032006@gmail.com>
2025-07-22 22:07:46 +0700

    nvim: add gdb debug adapter

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -7,5 +7,17 @@ return {
       { "<f11>", function() require("dap").step_into() end, desc = "Step Into" },
       { "<f12>", function() require("dap").step_out() end, desc = "Step Out" },
     },
+    opts = function()
+      local dap = require("dap")
+      dap.adapters.codelldb = nil
+      dap.configurations.c = nil
+      dap.configurations.cpp = nil
+
+      dap.adapters.cppdbg = {
+        id = "cppdbg",
+        type = "executable",
+        command = "OpenDebugAD7",
+      }
+    end,
   },
 }
