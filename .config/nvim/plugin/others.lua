b0cc44b7d2a5d0ed1bbf7a5241ab2b872912ee1a
nptrc <khoa05032006@gmail.com>
2025-08-01 08:18:35 +0700

    nvim: pin blink.cmp version

--- .config/nvim/plugin/others.lua
+++ .config/nvim/plugin/others.lua
@@ -21,23 +21,10 @@ later(function()
     end,
   })
 
-  local function build_blink(params)
-    vim.notify("Building blink.cmp", vim.log.levels.INFO)
-    local obj = vim.system({ "cargo", "build", "--release" }, { cwd = params.path }):wait()
-    if obj.code == 0 then
-      vim.notify("Building blink.cmp done", vim.log.levels.INFO)
-    else
-      vim.notify("Building blink.cmp failed", vim.log.levels.ERROR)
-    end
-  end
-
   add({
     source = "saghen/blink.cmp",
     depends = { "rafamadriz/friendly-snippets" },
-    hooks = {
-      post_install = build_blink,
-      post_checkout = build_blink,
-    },
+    checkout = "v1.6.0",
   })
 
   require("blink.cmp").setup({
