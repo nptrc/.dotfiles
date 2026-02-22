d966617b047a6d05cbea0bde2488275617bf3736
nptrc <khoa05032006@gmail.com>
2026-02-22 21:51:29 +0700

    update emacs config

--- .emacs
+++ .emacs
@@ -1,7 +1,23 @@
 (load-theme 'tango-dark t)
 
-(setq custom-file "~/.emacs.custom.el")
-(load custom-file)
+(defun save-all-buffers ()
+  (interactive)
+  (save-some-buffers t))
+(add-hook 'focus-out-hook 'save-all-buffers)
+
+(defun my/turn-off-vc-for-remote-files ()
+  (when (file-remote-p (buffer-file-name))
+    (setq-local vc-handled-backends nil)))
+(add-hook 'find-file-hook 'my/turn-off-vc-for-remote-files)
+
+(defun my/cleanup-buffer-file ()
+  (interactive)
+  (ignore-errors
+    (unless (or (equal major-mode 'makefile-mode)
+                (equal major-mode 'makefile-bsdmake-mode))
+      (untabify (point-min) (point-max)))
+    (delete-trailing-whitespace)))
+(add-hook 'before-save-hook #'my/cleanup-buffer-file)
 
 (defun my/duplicate-line ()
   (interactive)
@@ -19,16 +35,20 @@
 (global-set-key (kbd "C-c b")       'recompile)
 
 (global-set-key (kbd "C-c m")       'man)
+
+(require 'package)
+(package-initialize)
+
+(global-set-key (kbd "C-x g")       'magit-status)
+
 (global-set-key (kbd "C->")         'mc/mark-next-like-this)
 (global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C-'")         'mc/skip-to-next-like-this)
 (global-set-key (kbd "C-;")         'mc/skip-to-previous-like-this)
-(global-set-key (kbd "C-x g")       'magit-status)
 
-(require 'package)
-(package-initialize)
+(add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy)
 
 (global-set-key (kbd "C-.") 'company-complete)
 (setq completion-ignore-case t)
@@ -44,8 +64,6 @@
   (define-key eglot-mode-map (kbd "C-c l f") #'eglot-format)
   (define-key eglot-mode-map (kbd "C-c l x") #'flymake-show-buffer-diagnostics)
   (define-key eglot-mode-map (kbd "C-c l p") #'flymake-show-project-diagnostics)
-
-  (setq eglot-ignored-server-capabilities '(:inlayHintProvider))
 
   (add-to-list 'eglot-stay-out-of 'company-backends)
   (add-to-list 'eglot-server-programs
@@ -65,3 +83,6 @@
           (lambda ()
             (c-toggle-comment-style -1)
             (setq c-basic-offset 4)))
+
+(setq custom-file "~/.emacs.custom.el")
+(load custom-file)
--- .emacs.custom.el
+++ .emacs.custom.el
@@ -6,6 +6,8 @@
  ;; If there is more than one, they won't work right.
  '(backup-directory-alist '(("." . "~/emacs_backups")))
  '(blink-cursor-mode nil)
+ '(c-syntactic-indentation nil)
+ '(c-tab-always-indent nil)
  '(column-number-mode t)
  '(company-backends
    '((company-capf :separate company-dabbrev-code) company-files))
@@ -15,29 +17,36 @@
  '(company-minimum-prefix-length 1)
  '(company-selection-wrap-around t)
  '(company-tooltip-maximum-width 50)
- '(compilation-auto-jump-to-first-error t)
  '(compilation-max-output-line-length nil)
  '(compilation-scroll-output t)
  '(completion-category-overrides '((file (styles partial-completion))))
  '(completion-styles '(orderless basic))
+ '(create-lockfiles nil)
+ '(custom-enabled-themes '(dracula tango-dark))
+ '(custom-safe-themes
+   '("2d74de1cc32d00b20b347f2d0037b945a4158004f99877630afc034a674e3ab7"
+     default))
  '(default-frame-alist
    '((fullscreen . maximized) (font . "JetbrainsMono Nerd Font-15")))
  '(delete-by-moving-to-trash t)
+ '(delete-old-versions t)
  '(delete-selection-mode t)
  '(dired-dwim-target 'dired-dwim-target-recent)
- '(dired-listing-switches "-lah")
+ '(dired-listing-switches "-lavh --group-directories-first")
  '(dired-mouse-drag-files t)
  '(display-line-numbers-type 'relative)
  '(eglot-autoshutdown t)
  '(eglot-events-buffer-config '(:size 0 :format full))
- '(eglot-ignored-server-capabilities '(:inlayHintProvider))
+ '(eglot-ignored-server-capabilities '(:documentOnTypeFormattingProvider :inlayHintProvider))
  '(eglot-report-progress nil)
  '(enable-recursive-minibuffers t)
  '(global-auto-revert-mode t)
  '(global-company-mode t)
  '(global-display-line-numbers-mode t)
+ '(imenu-auto-rescan t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
+ '(kept-new-versions 6)
  '(marginalia-mode t)
  '(menu-bar-mode nil)
  '(minibuffer-prompt-properties '(read-only t face minibuffer-prompt cursor-intangible t))
@@ -47,11 +56,14 @@
      ("nongnu" . "https://elpa.nongnu.org/nongnu/")
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(company magit marginalia mason multiple-cursors orderless vertico
-             vundo yasnippet))
+   '(company devdocs dracula-theme magit marginalia mason
+             multiple-cursors orderless vertico vundo yasnippet))
  '(read-buffer-completion-ignore-case t)
  '(read-extended-command-predicate 'command-completion-default-include-p)
  '(read-file-name-completion-ignore-case t)
+ '(recentf-auto-cleanup 'never)
+ '(recentf-mode t)
+ '(remote-file-name-inhibit-locks t)
  '(repeat-mode t)
  '(save-place-mode t)
  '(savehist-mode t)
@@ -61,6 +73,7 @@
  '(trash-directory "~/.Trash")
  '(use-package-always-ensure t)
  '(use-short-answers t)
+ '(vertico-cycle t)
  '(vertico-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
