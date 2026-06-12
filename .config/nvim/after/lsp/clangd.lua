8b0fedf2b28e8f27c667f65c0c1382d021d20904
nptrc <khoa05032006@gmail.com>
2026-06-12 22:26:25 +0700

    nvim: improve clangd config for espidf

--- .config/nvim/after/lsp/clangd.lua
+++ .config/nvim/after/lsp/clangd.lua
@@ -1,34 +1,40 @@
 local clangd_cmd = (function()
-  local clangd_cmd_opts = { "clangd" }
+  local clangd_bin = "clangd"
 
   if vim.env.IDF_PATH then
     local base = vim.env.HOME .. "/.espressif/tools/esp-clang"
     local scandir = vim.uv.fs_scandir(base)
-    if not scandir then
-      return nil
-    end
 
-    local latest
-    while true do
-      local name = vim.uv.fs_scandir_next(scandir)
-      if not name then
-        break
+    if scandir then
+      local versions = {}
+      while true do
+        local name, type = vim.uv.fs_scandir_next(scandir)
+        if not name then break end
+
+        if name:match("^esp%-.+") and type == "directory" then
+          table.insert(versions, name)
+        end
       end
-      if name:match("^esp%-.+") then
-        latest = base .. "/" .. name .. "/esp-clang/bin/clangd"
+
+      table.sort(versions)
+
+      if #versions > 0 then
+        local target_bin = base .. "/" .. versions[#versions] .. "/esp-clang/bin/clangd"
+        if vim.uv.fs_access(target_bin, "X") then
+          clangd_bin = target_bin
+        end
       end
     end
-    clangd_cmd_opts = {
-      latest,
-    }
   end
 
-  return vim.list_extend(clangd_cmd_opts, {
+  return {
+    clangd_bin,
+    "--query-driver=**/*gcc,**/*g++,**/*clang,**/*clang++",
     "--background-index",
     "--header-insertion=never",
     "--completion-style=detailed",
     "--function-arg-placeholders=1",
-  })
+  }
 end)()
 
 return {
